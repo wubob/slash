4ad2f3d60fd8accdeb790bc6bcc02e686b3ba1e0
Rotem Yaari <vmalloc@gmail.com>
2018-01-16 11:44:42 +0200

    Add support for naming parameter variations (closes #590)

---
+++ tests/test_parametrization_labels.py
@@ -0,0 +1,22 @@
+from slash._compat import izip_longest
+
+
+def test_parametrization_labels(suite, suite_test):
+    param = suite_test.add_parameter()
+    param.add_labels()
+
+    @suite_test.append_body
+    def __code__(): # pylint: disable=unused-variable
+        # pylint: disable=undefined-variablepylint: disable=no-member, protected-access, undefined-variable
+        slash.context.result.data['params'] = locals().copy()
+        slash.context.result.data['params'].pop('self', None)
+
+    res = suite.run()
+
+    for label, value, result in izip_longest(
+            param.labels, param.values, res.get_all_results_for_test(suite_test)):
+        assert label
+        assert value
+        assert result
+        #assert result.test_metadata.variation.id[param.name] == label
+        assert result.data['params'] == {param.name: value}
